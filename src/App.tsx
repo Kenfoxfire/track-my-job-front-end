4bd320169321e5efeeb8077698b046889024fc49
[Obed Baltodano] <obedbaltodano23@homtail.com>
2024-07-12 13:17:25 -0600

    tailwind support and background colors added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    < >
+    
+    <div className="p-10 m-20  max-w-sm mx-auto color5 rounded-xl shadow-md space-y-4">
+      <h1 className="text-2xl font-bold">Hello, Finders!</h1>
+      <p className="text-black-700">Here's your personal app to find your perfect fit in a job.</p>
+    </div>
+    
+    
     </>
+    
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      color1: '#5290ce',
+      color2: '#6ca9da',
+      color3: '#87c2e7',
+      color4: '#a1daf3',
+      color5: '#bbf3ff',
+    },
+},
+  },
+  plugins: [],
+}
