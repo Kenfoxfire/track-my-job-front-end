97d1170af841e4a67edf1a0f58b6872f4ea2d114
[Obed Baltodano] <obedbaltodano23@homtail.com>
2024-09-09 17:52:00 -0600

    new redux service added

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
 import './index.css'
 import AppRouter from './Router/AppRouter'
+import { Provider } from "react-redux";
+import { store } from './store/store';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store} >
     <BrowserRouter>
     <AppRouter  />
     </BrowserRouter>
+    </Provider>
    </React.StrictMode>,
 )
